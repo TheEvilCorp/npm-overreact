83682aa3b0b010d56728c64fdc79f89f6d35e06a
Brian <bgrober@gmail.com>
2015-12-08 17:04:00 -0800

    aasdasd

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
 var exec = require('child_process').exec;
+var fs = require('fs');
 
-// console.log(process.argv.slice(2)[0]);
-exec(`curl -O https://overreact.herokuapp.com/zips/${process.argv.slice(2)[0]}.zip; unzip -o ${process.argv.slice(2)[0]}.zip;rm -rf ${process.argv.slice(2)[0]}.zip; echo cd into your project. If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!`, function(err, stdout) {
+var projectName = process.argv.slice(2).split('-')[0];
+fs.stat(`./${req.body.folderName}`, function(err, stats) {
+    if(!stats) {
+exec(`curl -O http://localhost:8000/${process.argv.slice(2)[0]}.zip; unzip -o OverReact.zip;rm -rf OverReact.zip; echo cd into your project. If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!`, function(err, stdout) {
   console.log(stdout);
 });
