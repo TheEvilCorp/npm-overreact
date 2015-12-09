01271273d3179b70ffe50e7cbc84239f56323082
Brian <bgrober@gmail.com>
2015-12-08 19:39:53 -0800

    will no longer overwrite the current working directory for Mac/Linux

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ var projectName = process.argv.slice(2)[0].split('-')[0];
 var append = 0;
 
 function unzipIt(project, counter){
-  fs.stat('./${project}', function(err, stats) {
+  fs.stat(`./${project}`, function(err, stats) {
     if(!stats) {
-      exec(`curl -O http://localhost:8000/zips/${process.argv.slice(2)[0]}.zip; unzip -o ${process.argv.slice(2)[0]}.zip;rm -rf ${process.argv.slice(2)[0]}.zip; echo cd into `"${project}"`. If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!`, function(err, stdout) {
+      return exec(`curl -O https://overreact.herokuapp.com/zips/${process.argv.slice(2)[0]}.zip; unzip -qq -d ./${project} ${process.argv.slice(2)[0]}.zip && f=(./${project}/*) && mv ./${project}/*/* ${project};rm -rf ./${project}/${projectName};rm -rf ${process.argv.slice(2)[0]}.zip; mv ./${project}/*/*; echo cd into ${project}${counter <= 0 ? '.' : ', your project name was taken in the cwd, thus the appended number.'} If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!;`, function(err, stdout) {
         console.log(stdout);
-      });
+      })
+      // console.log(process.cwd());
     } else {
-      counter++
-      project += counter;
+      counter++;
+      project = projectName + counter;
       unzipIt(project, counter);
-    };
+    }
   });
 }
 unzipIt(projectName, append);
