9934881657a0eb09639c365b4fb52b6fa401a55a
Brian <bgrober@gmail.com>
2015-12-17 11:03:09 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # npm-overreact
 
-<p><a href="https://overreact.herokuapp.com/">OverReact</a> is the best way to start any project that will use Reactjs. By using OverReact you can wireframe your UI and with a click have all of your files ready to go!</p>
+<p><a href="https://overreact.io/">OverReact</a> is the best way to start any project that will use React. By using OverReact you can wireframe your UI and with a click have all of your files ready to go!</p>
 
 To get started:
 ```npm install -g over-react```
 
-Then head over to <a href="https://overreact.herokuapp.com/">OverReact</a> and wireframe your UI. Once done, head over to your terminal and copy and paste the text provided
-
-```overreact <hash provided>```
+Then head over to <a href="https://overreact.io/">OverReact</a> and wireframe your UI. Once done, head over to your terminal and copy and paste the text provided.
--- index.js
+++ index.js
@@ -8,7 +8,17 @@ var append = 0;
 function unzipIt(project, counter){
   fs.stat(`./${project}`, function(err, stats) {
     if(!stats) {
-      return exec(`curl -O https://overreact.herokuapp.com/zips/${process.argv.slice(2)[0]}.zip; unzip -qq -d ./${project} ${process.argv.slice(2)[0]}.zip && f=(./${project}/*) && mv ./${project}/*/* ${project};rm -rf ./${project}/${projectName};rm -rf ${process.argv.slice(2)[0]}.zip; mv ./${project}/*/*; echo cd into ${project}${counter <= 0 ? '.' : ', your project name was taken in the cwd, thus the appended number.'} If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!;`, function(err, stdout) {
+      return exec(
+        //initiates a curl request to OverReact server for the requested zip file
+        //then unzips the file
+        //the unzipped file is nested, so move all of the grandchildren into the root folder and delete the nested folder
+        //delete the zip and echo
+        `curl -O https://overreact.herokuapp.com/zips/${process.argv.slice(2)[0]}.zip &&
+        unzip -qq -d ./${project} ${process.argv.slice(2)[0]}.zip &&
+        mv ./${project}/*/* ${project} &&
+        rm -rf ./${project}/${project} &&
+        rm -rf ${process.argv.slice(2)[0]}.zip &&
+        echo cd into ${project}${counter <= 0 ? '.' : ', your project name was taken in the cwd, thus the appended number.'} If you requested a starter kit,  run "npm install" then "npm start", then visit your site at localhost:3000. Enjoy!;`, function(err, stdout) {
         console.log(stdout);
       })
       // console.log(process.cwd());
